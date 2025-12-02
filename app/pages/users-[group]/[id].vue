5bc0135ca65c57f1a2c557f10eae2c0623db944c
李龙 <lilong2576@hanweb.com>
2025-12-02 11:44:08 +0800

    feat: 设置路由参数-添加用户详情页面

---
+++ app/pages/users-[group]/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>User - {{ route.params.group }} - {{ route.params.id }}</h1>
+    <h2>部门 - {{ route.params.group }}</h2>
+    <h2>用户ID - {{ route.params.id }}</h2>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
