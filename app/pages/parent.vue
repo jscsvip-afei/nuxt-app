904e8fa3be4e58eadab61b870974b64bd8988cce
李龙 <lilong2576@hanweb.com>
2025-12-03 10:28:50 +0800

    feat: 更新父页面和子页面，添加 useFetch 示例，优化用户登录判断逻辑

--- app/pages/parent.vue
+++ app/pages/parent.vue
@@ -1,28 +1,31 @@
 <template>
+   <ClientOnly>
   <h1>Parent Page</h1>
   <NuxtPage />
   <h2>使用插件</h2>
   <p>{{ $myPluginMethod('World') }}</p>
-  <a-button @click="handleMessage" type="primary">
-    父级页面按钮
-  </a-button>
+    <a-button @click="handleMessage" type="primary">
+      父级页面按钮
+    </a-button>
+  </ClientOnly>
 </template>
 <script setup>
 // 匿名中间件
 definePageMeta({
   middleware: defineNuxtRouteMiddleware((to, from) => {
     // 判断用户是否登录
-    let user = false; // 初始化未登录
-    // 获取sessionStorage中的用户登录状态
-    const userInfo = sessionStorage.getItem('userInfo');
-    if (userInfo) {
-      user = true; // 已登录
+    if (import.meta.client) {
+      let user = false; // 初始化未登录
+      // 获取sessionStorage中的用户登录状态
+      const userInfo = sessionStorage.getItem('userInfo');
+      if (userInfo) {
+        user = true; // 已登录
+      }
+      if (!user) {
+        // 如果未登录且不是去登录页，则重定向到首页
+        return navigateTo('/login');
+      }
     }
-    if (!user) {
-      // 如果未登录且不是去登录页，则重定向到首页
-      return navigateTo('/login');
-    }
-
   })
 });
 const {$message} = useNuxtApp()
--- app/pages/parent/child.vue
+++ app/pages/parent/child.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h2>Child Page</h2>
+    <!--  -->
+    useFetch 组合式 API 示例：
   </div>
 </template>
 
 <script setup>
-
+const { data, pending, error, refresh } = await useFetch('https://api.nuxt.com/modules?param1=value1&param2=value2', {
+  pick: ['modules','stats'],  // 只获取 modules 和 stats 字段
+  // server: false    // 仅在客户端运行 , 注释掉之后是不会在浏览器中看到网络请求 是因为它默认在服务端运行 ,如果开启 刷新的时候会有水合错误 ,如果在parent嵌套 ClientOnly中使用就不会有水合错误 这时候相当于在客户端运行 server:false
+});
+console.log("Fetched data:", data.value);
 </script>
