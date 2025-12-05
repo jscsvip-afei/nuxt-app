c9a10626e6bd6dab2d1d3e806b194b932af1ee82
李龙 <lilong2576@hanweb.com>
2025-12-05 15:10:28 +0800

    feat: 添加jsonwebtoken类型定义，更新认证中间件以验证token并存储用户信息

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,28 @@
+/**
+ * 1. 获取token，存入context( 上下文event.context.auth 其他接口可以使用 )
+ * 2. 验证token合法性 如果有token的话,则处理token 之后再验证token的值
+ */
+import jwt from "jsonwebtoken";
+
 export default defineEventHandler((event) => {
-  const token = getHeader(event, 'Authorization')
-  event.context.auth = { token }
+  let token = getHeader(event, 'Authorization');
+  console.log('auth token:', token);
+  if (token) {
+    // 去除 Bearer 前缀
+    token = token.replace('Bearer ', '');
+    const secret = "jianshu2024_secret";
+    
+    try {
+      // 验证并解析 token
+      const decoded = jwt.verify(token, secret);
+      // 将用户信息存入 context，供后续接口使用
+      console.log('decoded token:', decoded);
+      event.context.auth = {uid:  (decoded as any).uid};
+    } catch (error) {
+      // token 无效或已过期
+      event.context.auth = null;
+    }
+  } else {
+    event.context.auth = null;
+  }
 })
--- server/utils/helper/index.ts
+++ server/utils/helper/index.ts
@@ -6,3 +6,8 @@ export const responseJson = (code: number, msg: string, data: any) => {
     data
   }
 }
+
+export const getLoginUid = (event: any): number | null => {
+  // @ts-ignore
+  return event.context?.auth?.uid || null;
+} 
